86a7e4d75923ede2e29713f6faf6a252f72598be
PimPomUwU <daviparralopez@gmail.com>
2025-05-09 12:39:56 -0500

    Revert "Se relacionaron los modelos (has y belongsTo)"
    This reverts commit 5daf5ed502b873c875f0ace8f857be4c67703828.

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -7,15 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     //
-    public function area() {
-        return $this->belongsTo(Area::class);
-    }
-
-    public function courses() {
-        return $this->belongsToMany(Course::class);
-    }
-
-    public function trainingcenter() {
-        return $this->belongsTo(TrainingCenter::class);
-    }
 }
