5daf5ed502b873c875f0ace8f857be4c67703828
PimPomUwU <daviparralopez@gmail.com>
2025-05-09 12:29:54 -0500

    Se relacionaron los modelos (has y belongsTo)

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Area extends Model
 {
     //
+    public function courses() {
+        return $this->hasMany(Course::class);
+    }
+    public function teachers() {
+        return $this->hasMany(Teacher::class);
+    }
 }
--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     //
+    public function area() {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function courses() {
+        return $this->belongsToMany(Course::class);
+    }
+
+    public function trainingcenter() {
+        return $this->belongsTo(TrainingCenter::class);
+    }
 }
